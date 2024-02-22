dc3f346accfa5ce20abc13598b6534a2c7514933
drydenne14 <79269122+drydenne14@users.noreply.github.com>
2024-02-22 15:07:29 +0000

    trying to add sources

--- coffee_shop_drydenne/models/monthly_customers.sql
+++ coffee_shop_drydenne/models/monthly_customers.sql
@@ -1,5 +1,5 @@
 select 
   date_trunc(first_order_at, month) as date_month, 
   count(*) as new_customers
-from {{ref('customers')}}
+from {{ source('coffee_shop_drydenne', 'customers')}}
 group by date_month 
