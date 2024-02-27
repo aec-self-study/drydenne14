c20ba5457deaaafa917ad344c83f7e37295084be
drydenne14 <79269122+drydenne14@users.noreply.github.com>
2024-02-27 16:06:43 +0000

    o

--- coffee_shop_drydenne/models/monthly_customers.sql
+++ coffee_shop_drydenne/models/monthly_customers.sql
@@ -1,5 +1,5 @@
 select 
   date_trunc(first_order_at, month) as date_month, 
   count(*) as new_customers
-from {{ source('dbt_drydenne', 'customers')}}
+from dbt_drydenne.customers
 group by date_month
